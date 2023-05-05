edbac7c2dee0430ce0cf9c02abeb07bdd08d91d2
MajaRygard <maja.rygard@gmail.com>
2023-05-05 08:24:30 +0200

    levelactivity

--- app/src/main/java/com/example/driversschool/Level.java
+++ app/src/main/java/com/example/driversschool/Level.java
@@ -1,6 +1,23 @@
 package com.example.driversschool;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class Level  extends AppCompatActivity {
+    private NavController navController;
+    private Button easy;
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+
+        this.navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        this.easy = (Button)findViewById(R.id.easy);
+
+
+    }
 }
