0a7dafa18fbbc3270d590cab13667b35d14f8a19
MajaRygard <maja.rygard@gmail.com>
2023-05-08 11:19:21 +0200

    Ljud med intruktioner när spelet börjar

--- app/src/main/java/com/example/driversschool/Popup.java
+++ app/src/main/java/com/example/driversschool/Popup.java
@@ -1,6 +1,7 @@
 package com.example.driversschool;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,21 +11,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Popup  extends AppCompatActivity {
     private Button cross;
+    private MediaPlayer mp;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
 
+        this.mp = MediaPlayer.create(Popup.this, R.raw.instruktion);
         this.cross = (Button) findViewById(R.id.cross);
 
 
         cross.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mp.start();
                 startActivity(new Intent(Popup.this, GameActivity.class));
             }
 
         });
+
+
     }
 }
